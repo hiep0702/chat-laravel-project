34e7e35cdf23effd744cc8cfa37407cb6b4066b4
Tran-Tuan-Hiep <trantuanhiep07022001@gmail.com>
2023-04-28 10:03:19 +0700

    create database

---
+++ database/migrations/2023_04_28_023817_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chat_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
+            $table->timestamps();
+
+            // Khóa ngoại liên kết đến bảng 'users' và bảng 'chats'
+            $table->foreign('chat_id')->references('id')->on('chats');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
